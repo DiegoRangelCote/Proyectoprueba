68fab5f6184ebb5018c075784dc01af8a8aa0ac1
DiegoRangelCote <drangel4@ibero.edu.co>
2023-07-31 18:34:40 -0500

    datos que me tenian represado en el trabajo del proyecto

--- IBERO_BACKEND/config.js
+++ IBERO_BACKEND/config.js
@@ -5,9 +5,5 @@ config.ipbd = "127.0.0.1"
 config.clavesesion = "holamundo"
 
 
-<<<<<<< HEAD
 
 module.exports.config=config
-=======
-module.exports.config = config
->>>>>>> 8ec86934cbc2c8388c0a2789474133d17bdf9003
